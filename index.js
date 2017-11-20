a46e2c88e323ce11c9e01d7bd157fe26f989be37
Ginny Munson <virginia.munson@gmail.com>
2017-11-20 23:27:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,21 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let itemInCart = false;
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      l--;
+    }
+  }
+
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+
+  return cart;
 }
 
 function placeOrder(cardNumber) {
